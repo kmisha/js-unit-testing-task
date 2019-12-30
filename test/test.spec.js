ed7d87a195fda7f8f1bd0600672f00cf51f178ee
Mikhail Kolesnikov <misha@kmisha.ru>
2019-12-30 19:39:50 +0400

    added code from task WIP

--- test/test.spec.js
+++
@@ -1,5 +0,0 @@
-describe('calc',() =>{
-    it('should return correct value', () => {
-        expect(calc(1,1)).toBe(2)
-    })
-});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry  : './src/index.js',
+    output : {
+        path     : path.resolve(__dirname, 'lib'),
+        filename : 'index.js'
+    },
+    module : {
+        rules: [ {
+                test   : /.js$/,
+                loader : 'babel-loader'
+            }
+        ]
+    }
+};
+
